3fe46aa9aa8a18c966b8a0c53a09449281eddd12
gmbreguet <guy-michel.breguet@heig-vd.ch>
2024-09-23 17:00:41 +0200

    2024-09-23 17:00:41 - GMB

--- 5_Classe_PRG1-E/Demos_PRG1-E/2024-09-23.cpp
+++ 5_Classe_PRG1-E/Demos_PRG1-E/2024-09-23.cpp
@@ -1,16 +1,54 @@
 #include <iostream>
 #include <cstdlib>
+#include <array>
 #include <string>
 #include <iomanip>
 
 using namespace std;
 
-int main() {
+int fonction () {
+   cout << "dans ma fonction ..." << endl;
+   return 42;
+}
 
+int main() {
    int x;
    int y;
 
+   cout << x << " " << y << endl;
+
    x = y = 42;
+
+   cout << x << " " << y << endl;
+
+   cout << fonction() << endl;
+
+   42;
+
+   cout << (5.0 * 2.0) << endl;
+   cout << (5   * 2  ) << endl;
+   cout << (5.0 * 2  ) << endl;
+
+   int nbre = 7;
+
+   cout <<   nbre           << endl;
+   cout << ++nbre           << endl;
+   cout << nbre++   << nbre << endl;
+
+   const int i = -7;
+//   cout << (++i * --i) << endl;
+
+   cout << -i << i << endl;
+
+   cout << boolalpha << (i == -7) << endl;
+
+   cout << setprecision(20) << 10.0 / 3.0 << endl;
+   cout << (10.0 / 3.0 == 3.333) << endl;
+
+   string mot1 = "maison";
+   string mot2 = "mais";
+
+   cout << (mot1 < mot2) << endl;
 
    return EXIT_SUCCESS;
 }
