7a0cd54da3650d6631690b26f565b1bb9ae4f3f6
gmbreguet <guy-michel.breguet@heig-vd.ch>
2024-11-25 11:09:15 +0100

    2024-11-25 11:09:15 - GMB

--- Classe_PRG1-B/Demos_PRG1-B/2024-11-25.cpp
+++ Classe_PRG1-B/Demos_PRG1-B/2024-11-25.cpp
@@ -5,18 +5,6 @@
 
 using namespace std;
 
-
-ostream& operator<< (ostream& os, const vector<int>& v) {
-   os << "[";
-   for (size_t i=0; i<v.size(); ++i) {
-      if (i) cout << ", ";
-      os << v[i];
-   }
-   os << "]";
-   return os;
-}
-
-
 int main() {
 
    Pair<int> p {1, 2};
@@ -24,21 +12,24 @@ int main() {
    cout << p.first << " " << p.second << endl;
    echanger(p);
    cout << p.first << " " << p.second << endl;
-
    cout << endl;
 
    Vector<int> v = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
 
    parcourir<int>(v, afficher<int>);
-   parcourir<int>(v, echanger<int>);
-   parcourir<int>(v, afficher<int>);
-
+   parcourir<int>(v, echanger<int>);   cout << endl;
+   parcourir<int>(v, afficher<int>);   cout << endl;
    cout << endl;
 
-   vector<int> v2 {1, 2, 3, 4, 5, 6};
+   vector<int> v1 {1, 2, 3, 4, 5, 6, 7, 8};
+   cout << v1 << endl;
+   cout << endl;
+
+   Vector<int> v2 {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+   cout << v2 << endl;
+   parcourir<int>(v2, echanger<int>);
    cout << v2 << endl;
 
-   cout << endl;
    return EXIT_SUCCESS;
 }
 
--- Classe_PRG1-B/Demos_PRG1-B/2024-11-25_header.h
+++ Classe_PRG1-B/Demos_PRG1-B/2024-11-25_header.h
@@ -4,10 +4,12 @@
 #include <utility>
 
 //----------------------------------------
-
+// déclarations
+//----------------------------------------
 template <typename T>
 using Pair   = std::pair<T, T>;
 
+//----------------------------------------
 template <typename T>
 using Vector = std::vector<Pair<T>>;
 
@@ -23,7 +25,16 @@ void afficher(const Pair<T>& p);
 template <typename T, typename Fct>
 void parcourir(Vector<T>& v, Fct fct);
 
+//----------------------------------------
+template <typename T>
+std::ostream& operator<< (std::ostream& os, const Pair<T>& p);
 
+//----------------------------------------
+template <typename T>
+std::ostream& operator<< (std::ostream& os, const std::vector<T>& v);
+
+//----------------------------------------
+// définitions
 //----------------------------------------
 template <typename T>
 void echanger(Pair<T>& p) {
@@ -35,16 +46,32 @@ void echanger(Pair<T>& p) {
 //----------------------------------------
 template <typename T>
 void afficher(const Pair<T>& p) {
-   std::cout << p.first << " " << p.second;
+   std::cout << p;
 }
 
 //----------------------------------------
 template <typename T, typename Fct>
 void parcourir(Vector<T>& v, Fct fct) {
-   for (Pair<T>& p : v) {
+   for (Pair<T>& p : v)
       fct(p);
-      std::cout << " ";
-   }
 }
 
+//----------------------------------------
+template <typename T>
+std::ostream& operator<< (std::ostream& os, const Pair<T>& p) {
+   os << "(" << p.first << ", " << p.second << ")";
+   return os;
+}
+
+//----------------------------------------
+template <typename T>
+std::ostream& operator<< (std::ostream& os, const std::vector<T>& v) {
+   os << "[";
+   for (size_t i=0; i<v.size(); ++i) {
+      if (i) os << ", ";
+      os << v[i];
+   }
+   os << "]";
+   return os;
+}
 #endif
